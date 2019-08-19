19a8601e143dba78c0842d6bdea576217685a977
mpranali <52242355+mpranali@users.noreply.github.com>
2019-08-19 18:53:23 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$('.serbox').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#testimonial-slider').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+ 
+
